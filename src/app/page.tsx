f2ba5b29c7183189ca8977dbfca4dcb8445ea21e
bienfait shomari <bienfaitshm@gmail.com>
2023-06-11 14:42:45 +0200

    adding open graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,54 +7,46 @@ import PlayListCard from "@/components/PlayListCard";
 import Albums from "@/components/Albums";
 import PodCast from "@/components/PodCast";
 import Player from "@/components/Player";
-import Opengraph from "@/components/Opengraph";
-
-const description =
-    "Un Template Nextjs et Typescript inspiré par une image sur Pinterest, par l'équipe Jungo, (Dev, bienfait Shomari)";
 
 export default function Home() {
     return (
-        <main>
-            <Opengraph
-                title="Template Dailydose Music"
-                description={description}
-                image="./images/img1.jpg"
-                imageType=".jpg"
-            />
-            <div className="">
-                <div className="bg-slate-50 max-h-screen fixed left-0 top-0 bottom-0 w-[250px]">
-                    <SideBar />
-                </div>
-                <div className=" relative ml-[250px]">
-                    <Navbar />
-                    <MainContainer
-                        mainContent={<MainImage />}
-                        subContent={
-                            <>
-                                <PlayListCard
-                                    title="CHART"
-                                    src="/images/img1.jpg"
-                                />
-                                <PlayListCard
-                                    title="New Release"
-                                    src="/images/img2.jpg"
-                                />
-                                <PlayListCard
-                                    title="Discover"
-                                    src="/images/img1.jpg"
-                                />
-                            </>
-                        }
-                        topCharts={<TopCharts />}
-                        album={<Albums />}
-                        podcast={<PodCast />}
-                    />
-                    <div className="mb-20" />
-                    <div className=" ml-[250px] fixed bottom-0 right-0 left-0 p-4 bg-white/50 backdrop-blur-md">
-                        <Player />
+        <div>
+            <main>
+                <div className="">
+                    <div className="bg-slate-50 max-h-screen fixed left-0 top-0 bottom-0 w-[250px]">
+                        <SideBar />
+                    </div>
+                    <div className=" relative ml-[250px]">
+                        <Navbar />
+                        <MainContainer
+                            mainContent={<MainImage />}
+                            subContent={
+                                <>
+                                    <PlayListCard
+                                        title="CHART"
+                                        src="/images/img1.jpg"
+                                    />
+                                    <PlayListCard
+                                        title="New Release"
+                                        src="/images/img2.jpg"
+                                    />
+                                    <PlayListCard
+                                        title="Discover"
+                                        src="/images/img1.jpg"
+                                    />
+                                </>
+                            }
+                            topCharts={<TopCharts />}
+                            album={<Albums />}
+                            podcast={<PodCast />}
+                        />
+                        <div className="mb-20" />
+                        <div className=" ml-[250px] fixed bottom-0 right-0 left-0 p-4 bg-white/50 backdrop-blur-md">
+                            <Player />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </div>
     );
 }
